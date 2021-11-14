0edb1aadf09891c3344c08b24c56a927e1c65331
chtnpatil14 <root@test.km3eijumdqlujj1fhw4coy1mkc.ax.internal.cloudapp.net>
2021-11-14 05:39:54 +0000

    first commit

---
+++ terraform/linuxvm/vm.tf
@@ -0,0 +1,98 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "c876d590-171e-4369-8944-1fe2d08d7bf3"
+  client_id       = "5d3cf6e9-233e-4fcf-b9cf-01fda9a23de2"
+  client_secret   = "P9i4b__JC87FLjxdOwwkVrx-AEN7..YI.w"
+  tenant_id       = "8141551e-f178-48c9-ab30-d5f6670a72c4"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.virtual_network_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  count = var.number_of_vms
+  name                = "pip-${count.index}"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = var.network_security_group_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "example" {
+  count = var.number_of_vms
+  name                = "nic-${count.index}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.example.*.id[count.index]
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "association" {
+  count = var.number_of_vms
+  network_interface_id      = azurerm_network_interface.example.*.id[count.index]
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  count = var.number_of_vms
+  name                = "vm-${count.index}"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = var.username
+  admin_password      = "Pa$$w0rd1234!"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.example.*.id[count.index],
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
